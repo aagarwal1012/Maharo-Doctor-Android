00178270ce4b7f7280850d2954e999923bb5c52d
ac-alpha <ashutoshchaubey2999@gmail.com>
2018-07-22 16:08:52 +0530

    Added make appointment activity layout

---
+++ app/src/main/java/in/ashutoshchaubey/maharodoctor/CurrentStatusActivity.java
@@ -0,0 +1,13 @@
+package in.ashutoshchaubey.maharodoctor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CurrentStatusActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_current_status);
+    }
+}
--- app/src/main/java/in/ashutoshchaubey/maharodoctor/MainActivity.java
+++ app/src/main/java/in/ashutoshchaubey/maharodoctor/MainActivity.java
@@ -1,7 +1,9 @@
 package in.ashutoshchaubey.maharodoctor;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.fab_make_appointment).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, MakeAppointmentActivity.class));
+            }
+        });
+
     }
 }
